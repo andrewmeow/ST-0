cbc80fbc14ed8d916e9a9d0ab0d06b10284b1494
andrewmeow <91527374+andrewmeow@users.noreply.github.com>
2025-02-22 13:05:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-  if (b == 0) return a;
-  return gcd(b, a % b);
+  while (b != 0) {
+    int t = b;
+    b = a % b;
+    a = t;
+  }
+  return a;
 }
